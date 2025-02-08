3c3502ae444ff5466eacb5350a9c4bbb279b5116
Mo-b066 <119445295+Mo-b066@users.noreply.github.com>
2025-02-08 14:28:20 +0100

    Übung 2: Aufgabe 7a)

---
+++ Uebung/Uebung 2/src/A7.java
@@ -0,0 +1,14 @@
+public class A7 {
+
+    /*
+    Auto (Strimg typ, String farbe, int baujahr, int kmStand, int inspektionsIntervall ){
+
+        this.typ=typ;
+        this.baujahr=baujahr;
+        this.kmStand=kmStand;
+        this.inspektionsIntervall=inspektionsIntervall;
+    }
+*/
+
+
+}
